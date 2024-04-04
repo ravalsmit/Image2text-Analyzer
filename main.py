9512a994fd7e7a50bf0dd97020b4387939ae5f31
Smit Raval <72595063+ravalsmit@users.noreply.github.com>
2024-04-04 12:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,38 +6,11 @@ from collections import Counter
 import re
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from textblob import TextBlob
+import difflib
 
 # Set path to Tesseract executable
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def extract_text(image):
-    try:
-        text = tess.image_to_string(image)
-        return text.strip()
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-        st.warning("Please try uploading another image or check your internet connection.")
-        return None
-
-def extract_keywords(text):
-    # Remove stop words
-    custom_stopwords = set(ENGLISH_STOP_WORDS)
-    words = re.findall(r'\b\w+\b', text.lower())
-    words = [word for word in words if word not in custom_stopwords]
-
-    # Filter out non-keywords based on specific conditions
-    keywords = []
-    for word in words:
-        # Exclude single-character words and numeric values
-        if len(word) > 3 and not word.isdigit():
-            keywords.append(word)
-
-    # Get word frequencies
-    word_freq = Counter(keywords)
-
-    # Get top 30 keywords based on frequency
-    top_keywords = [word for word, freq in word_freq.most_common(30)]
-    return top_keywords, word_freq
 
 def analyze_sentiment(text):
     blob = TextBlob(text)
@@ -49,23 +22,48 @@ def analyze_sentiment(text):
     else:
         return "Neutral"
 
+
+def extract_text(image):
+    try:
+        text = tess.image_to_string(image)
+        return text.strip()
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        st.warning("Please try uploading another image or check your internet connection.")
+        return None
+
+
+def spell_check(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    corrected_text = []
+    for word in words:
+        corrected_word = difflib.get_close_matches(word, ENGLISH_STOP_WORDS, n=1)
+        corrected_text.append(corrected_word[0] if corrected_word else word)
+    return ' '.join(corrected_text)
+
+
+def extract_keywords(text):
+    # Tokenize the text into words
+    words = re.findall(r'\b\w+\b', text.lower())
+
+    # Remove stop words
+    custom_stopwords = set(ENGLISH_STOP_WORDS)
+    words = [word for word in words if word not in custom_stopwords]
+
+    # Filter out non-keywords based on specific conditions
+    keywords = [word for word in words if len(word) > 3 and not word.isdigit()]
+
+    # Get word frequencies
+    word_freq = Counter(keywords)
+
+    # Get top 30 keywords based on frequency
+    top_keywords = [word for word, freq in word_freq.most_common(30)]
+
+    return top_keywords, word_freq
+
+
 def main():
     st.title("Image2Text Analyzer")
-    st.markdown(
-        """
-        <style>
-        .reportview-container {
-            background: url("https://example.com/background_image.jpg");
-            background-size: cover;
-        }
-        .sidebar .sidebar-content {
-            background: linear-gradient(to right, #b3ffab, #12fff7);
-            color: black;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
     # Upload Image
     st.header("Choose an image...")
@@ -81,12 +79,17 @@ def main():
             extracted_text = extract_text(image)
 
             if extracted_text:
-                # Analyze sentiment of extracted text
-                sentiment = analyze_sentiment(extracted_text)
+                # Perform spell-checking
+                corrected_text = spell_check(extracted_text)
+                # st.write("Corrected Text:")
+                # st.write(corrected_text)
+
+                # Analyze sentiment of corrected text
+                sentiment = analyze_sentiment(corrected_text)
                 st.subheader(f"Sentiment: {sentiment}")
 
                 # Provide a download link for the extracted text and keywords
-                col1, col2 = st.columns(2)
+                col1, col2, col3 = st.columns(3)
                 with col1:
                     st.download_button(
                         label="Download Extracted Text",
@@ -95,8 +98,15 @@ def main():
                         mime="text/plain"
                     )
                 with col2:
+                    st.download_button(
+                        label="Download Corrected Text",
+                        data=corrected_text,
+                        file_name="corrected_text.txt",
+                        mime="text/plain"
+                    )
+                with col3:
                     # Extract keywords from extracted text
-                    keywords, word_freq = extract_keywords(extracted_text)
+                    keywords, word_freq = extract_keywords(corrected_text)
                     keywords_text = "\n".join(keywords)
                     st.download_button(
                         label="Download Keywords",
@@ -104,6 +114,7 @@ def main():
                         file_name="extracted_keywords.txt",
                         mime="text/plain"
                     )
+
 
 if __name__ == "__main__":
     main()
